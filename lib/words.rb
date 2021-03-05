260fae6902bf15eb5c0a2e752f1d4502cb10e037
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-05 09:04:11 -0800

    complete basic logic for words.rb

--- lib/words.rb
+++ lib/words.rb
@@ -15,3 +15,17 @@ class Words
   def self.all
     @@words.values
   end
+
+  def save
+    @@words[self.id] = Word.new(self.spelling, self.id)
+  end
+
+  def update(spelling)
+    self.spelling = spelling
+    @@words[self.id] = Word.new(self.spelling, self.id)
+  end
+
+  def delete
+    @@words.delete(self.id)
+  end
+end
